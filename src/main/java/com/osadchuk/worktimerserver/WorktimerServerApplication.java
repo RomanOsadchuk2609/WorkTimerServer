4f3d551f4deb135500a0b26bac22aba4872a5f7e
Roman Osadchuk <osadchuk.roma.26@gmail.com>
2020-01-18 12:06:56 +0200

    init commit

---
+++ src/main/java/com/osadchuk/worktimerserver/WorktimerServerApplication.java
@@ -0,0 +1,13 @@
+package com.osadchuk.worktimerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorktimerServerApplication.class, args);
+    }
+
+}
